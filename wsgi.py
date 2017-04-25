3b5c3b3e51940dcf36e886e5b4ffaeef6d2bf86a
LSDB2 <1099661784@qq.com>
2017-04-25 23:02:56 +0800

    修改wsgi
    修改wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,13 @@
-import app as bbs
+#!/usr/bin/env python3
 
-app = bbs.configured_app()
+import sys
+from os.path import abspath
+from os.path import dirname
+import app
+
+
+sys.path.insert(0, abspath(dirname(__file__)))
+application = app.app
 
 # gunicorn appcorn:app
 # nohup gunicorn -b '0.0.0.0:80' appcorn:app &
