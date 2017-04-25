13e5214963608fd421312c6693e0a2d6ebf20131
LSDB2 <1099661784@qq.com>
2017-04-25 22:35:03 +0800

    修改app和config.py
    修改了一些东西

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 import time
 from models import db
-# 这里 import 具体的 Model 类是为了给 migrate 用
-# 如果不 import 那么无法迁移
-# 这是 SQLAlchemy 的机制
 from models.user import User
 from models.node import Node
 from models.topic import Topic
@@ -52,7 +49,6 @@ def configured_app():
     return app
 
 
-# 自定义的命令行命令用来运行服务器
 @manager.command
 def server():
     print('server run')
@@ -78,4 +74,4 @@ if __name__ == '__main__':
     configure_app()
     manager.run()
 
-# gunicorn -b '0.0.0.0:80' redischat:app
+
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-secret_key = 'web18 bbs'
+secret_key = 'webbbs'
 user_file_director = r'C:\Users\Administrator\Desktop\简历\bbs\user_image'
 accept_user_file_type = ['jpg', 'gif', 'png']
